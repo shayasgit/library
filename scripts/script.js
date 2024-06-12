6a50f6f105523f3b6428cddc67b50f382cdbd537
Shayas R.A <shayas.contact@gmail.com>
2024-06-12 18:36:07 +0530

    HTML and CSS UI features are added

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 console.log("Library | The Odin Project");
 
-let formBox = document.querySelector('#form-box')
-let addBtn = document.querySelector('#add-btn');
-addBtn.addEventListener('click', () => {
-    formBox.showModal();
+let formBox = document.querySelector("#form-box");
+let addBtn = document.querySelector("#add-btn");
+addBtn.addEventListener("click", () => {
+  formBox.showModal();
 });
 
-let closeBtn = document.querySelector('.close');
-closeBtn.addEventListener('click', () => {
-    formBox.close();
-})
+let closeBtn = document.querySelector(".close");
+closeBtn.addEventListener("click", () => {
+  formBox.close();
+});
