d4dbd3488aeed462acbccaa9c277842d68d53c4e
Shayas R.A <shayas.contact@gmail.com>
2024-06-12 13:00:17 +0530

    Added Add Book btn and styled added event listener to add book btn to show form dialog box

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
 console.log("Library | The Odin Project");
+
+let formBox = document.querySelector('#form-box')
+let addBtn = document.querySelector('#add-btn');
+addBtn.addEventListener('click', () => {
+    formBox.showModal();
+});
